a14f05050b7164a6bc9da1817f8ec0a5b80b240d
David Goldberg <davi.gold@hotmail.com>
2019-04-16 13:24:40 +0300

    first commit

--- FlightSimulator/MainWindow.xaml.cs
+++ FlightSimulator/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace FlightSimulator
+namespace FlightSimulator1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
