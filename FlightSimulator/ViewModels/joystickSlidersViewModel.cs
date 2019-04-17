5d25eb17b378faae827b7b5fe64e826f26b99a00
Daniella Kirshenbaum <daniella.kirshenbaum@gmail.com>
2019-04-17 17:27:07 +0300

    td

---
+++ FlightSimulator/ViewModels/joystickSlidersViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightSimulator.Model;
+
+namespace FlightSimulator.ViewModels
+{
+    class joystickSlidersViewModel
+    {
+        public float throttle
+        {
+            set
+            {
+                ClientCommands.Instance.sendStream("set controls/flight/throttle " + value);
+            }
+        }
+
+        public float rudder
+        {
+            set
+            {
+                ClientCommands.Instance.sendStream("set controls/flight/rudder " + value);
+            }
+        }
+
+        public float aileron
+        {
+            set
+            {
+                ClientCommands.Instance.sendStream("set controls/flight/aileron " + value);
+            }
+        }
+
+        public float elevator
+        {
+            set
+            {
+                ClientCommands.Instance.sendStream("set controls/flight/elevator " + value);
+            }
+        }
+
+    }
+}
