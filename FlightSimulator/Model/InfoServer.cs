a335bc172ed5a8e3a0bb238ca2447261f3de89e1
Daniella Kirshenbaum <daniella.kirshenbaum@gmail.com>
2019-04-17 15:05:00 +0300

    connect

---
+++ FlightSimulator/Model/InfoServer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightSimulator.ViewModels;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace FlightSimulator.Model
+{
+    //class which connects connection for updating information
+    class InfoServer : BaseNotify
+    {
+        //the client we're listening to
+        TcpClient client;
+        bool isConnected;
+        Thread listenThread;
+
+        public double lon
+        {
+            set
+            {
+                lon = value;
+            }
+            get
+            {
+                return lon;
+            }
+        }
+
+        private static InfoServer m_Instance = null;
+        public static InfoServer Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    m_Instance = new InfoServer();
+                }
+                return m_Instance;
+            }
+        }
+        public InfoServer()
+        {
+            isConnected = true;
+        }
+
+        //server side connection
+        public void connect()
+        {
+            //no need for try and catch because of assigment instructions
+            TcpListener server = null;
+            int port = ApplicationSettingsModel.Instance.FlightCommandPort;
+            IPAddress ipAd = IPAddress.Parse(ApplicationSettingsModel.Instance.FlightServerIP);
+            //creating server
+            server = new TcpListener(ipAd, port);
+            // Thread listenThread = new Thread(server.Start);
+            server.Start();
+            //tcp client for this server
+            client = server.AcceptTcpClient();
+            listenThread = new Thread(() =>
+            {
+                //NOT SURE IF I NEED THIS
+                Thread.CurrentThread.IsBackground = true;
+                /* run your code here */
+                listenAndRead();
+            });
+            listenThread.Start();
+            //need to check when to close thread
+        }
+
+        public void listenAndRead()
+        {
+            Byte[] bytes;
+            NetworkStream ns = client.GetStream();
+            //reading from client
+            while (isConnected)
+            {
+                if (client.ReceiveBufferSize > 0)
+                {
+                    bytes = new byte[client.ReceiveBufferSize];
+                    ns.Read(bytes, 0, client.ReceiveBufferSize);
+                    //msg = longitude-deg, latitude-deg 
+                    string msg = Encoding.ASCII.GetString(bytes);
+                    lon = float.Parse(msg.Split('-')[0]);
+                    lon = float.Parse(msg.Split('-')[1]);
+                }
+            }
+            ns.Close();
+            client.Close();
+        }
+
+        public void disconnect()
+        {
+            //will stop while loop
+            isConnected = false;
+            client.Close();
+        }
+    }
+}
---
+++ FlightSimulator/ViewModels/SetConDisCon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightSimulator.Model;
+using System.Windows.Input;
+using System.Threading;
+
+namespace FlightSimulator.ViewModels
+{
+    class SetConDisCon
+    {
+        private ICommand _connect;
+        public ICommand connect
+        {
+            get
+            {
+                return _connect ?? (_connect = new CommandHandler(() => commandClicked()));
+            }
+
+        }
+        //calling connect in model
+        public void commandClicked()
+        {
+            //if is already connected, need to close 
+            //the previous connection and create a new connections
+
+            //else
+            new Thread(() =>
+            {
+                //creating server side connection
+                InfoServer.Instance.connect();
+                //need to add connect for client side
+            }).Start();
+        }
+    }
+}
