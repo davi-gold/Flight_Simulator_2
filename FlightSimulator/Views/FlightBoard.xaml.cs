5da355ce6dd0c718f1271857803e7485f10d09f9
Daniella Kirshenbaum <daniella.kirshenbaum@gmail.com>
2019-04-18 16:10:02 +0300

    sdgfd

--- FlightSimulator/Views/FlightBoard.xaml.cs
+++ FlightSimulator/Views/FlightBoard.xaml.cs
@@ -47,7 +47,7 @@ namespace FlightSimulator.Views
         {
             if (e.PropertyName.Equals("Lat") || e.PropertyName.Equals("Lon"))
             {
-                Point p1 = new Point(0, 0);            // Fill here!
+                Point p1 = new Point(fb.Lat,fb.Lon);            // Fill here!
                 planeLocations.AppendAsync(Dispatcher, p1);
             }
         }
