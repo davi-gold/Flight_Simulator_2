bf2f920fbb719b80bb21ab2b8983f3c15f26bda4
Daniella Kirshenbaum <daniella.kirshenbaum@gmail.com>
2019-04-20 23:10:09 +0300

    adsf

--- FlightSimulator/Model/InfoServer.cs
+++ FlightSimulator/Model/InfoServer.cs
@@ -65,9 +65,12 @@ namespace FlightSimulator.Model
         //server side connection
         public void connect()
         {
-            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ApplicationSettingsModel.Instance.FlightServerIP),
+            /*IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ApplicationSettingsModel.Instance.FlightServerIP),
        ApplicationSettingsModel.Instance.FlightInfoPort);
-            listener = new TcpListener(ep);
+            listener = new TcpListener(ep);*/
+            Int32 port = ApplicationSettingsModel.Instance.FlightInfoPort;
+            IPAddress ip = IPAddress.Parse(ApplicationSettingsModel.Instance.FlightServerIP);
+            listener = new TcpListener(ip, port);
             listener.Start();
             // Console.WriteLine("Waiting for client connections...");
             client = listener.AcceptTcpClient();
@@ -77,27 +80,56 @@ namespace FlightSimulator.Model
             thread.Start();
         }
 
+        //this should be simluator sending me the info but i don't think it's working
         public void listenAndRead(TcpClient client, TcpListener listener)
         {
-            Byte[] bytes;
-            NetworkStream ns = client.GetStream();
-            //reading from client
+             /*Byte[] bytes;
+             NetworkStream ns = client.GetStream();
+             //reading from client
+             while (isConnected)
+             {
+                 if (client.ReceiveBufferSize > 0)
+                 {
+                     bytes = new byte[client.ReceiveBufferSize];
+                     ns.Read(bytes, 0, client.ReceiveBufferSize);
+                     //msg = longitude-deg, latitude-deg 
+                     string msg = Encoding.ASCII.GetString(bytes);
+                     if (msg.Contains(","))
+                     {
+                         lon = float.Parse(msg.Split(',')[0]);
+                         lat = float.Parse(msg.Split(',')[1]);
+                     }
+                 }
+             }
+             ns.Close();
+            client.Close();
+            listener.Stop();*/
+             
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
+            NetworkStream stream = client.GetStream();
+            int i;
             while (isConnected)
             {
-                if (client.ReceiveBufferSize > 0)
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                 {
-                    bytes = new byte[client.ReceiveBufferSize];
-                    ns.Read(bytes, 0, client.ReceiveBufferSize);
-                    //msg = longitude-deg, latitude-deg 
-                    string msg = Encoding.ASCII.GetString(bytes);
-                    if (msg.Contains(","))
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
+                    if (data.Contains(","))
                     {
-                        lon = float.Parse(msg.Split(',')[0]);
-                        lat = float.Parse(msg.Split(',')[1]);
+                        lon = float.Parse(data.Split(',')[0]);
+                        lat = float.Parse(data.Split(',')[1]);
                     }
                 }
+
             }
-            ns.Close();
+            stream.Close();
             client.Close();
             listener.Stop();
         }
